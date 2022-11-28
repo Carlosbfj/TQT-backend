6ecc12f740549cf7ed8ca0b584dfb17f6106d06c
Carlos Fernandez <bernardofdez2011@gmail.com>
2022-11-28 09:46:37 -0600

    Dev started here

---
+++ index.js
@@ -0,0 +1,13 @@
+const fs = require("fs/promises");
+const express = require("express");
+const cors = require("cors");
+const _ = require("lodash");
+const { v4: uuid } = require("uuid");
+
+const app = express();
+
+app.get("/ping", (req, res) => {
+    res.send("pong");
+});
+
+app.listen(8080, () => console.log("API server is runnin.."));
